7f62b4cf9dde1f6ea82eed7a704f2b6110d258df
Donal Noye <leekeiz49@gmail.com>
2023-06-13 00:29:07 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shirt-three.js",
   plugins: [react()],
+  base: "/shirt-three.js/",
 })
